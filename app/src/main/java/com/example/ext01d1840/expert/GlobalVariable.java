121d0610f651f4bcea30d622109f5bcf079e6f5b
aytulyesil <aytulyesil@gmail.com>
2016-07-25 16:59:39 +0300

    globalVariable.class eklendi

---
+++ app/src/main/java/com/example/ext01d1840/expert/GlobalVariable.java
@@ -0,0 +1,21 @@
+package com.example.ext01d1840.expert;
+
+import android.app.Application;
+
+/**
+ * Created by EXT01D1840 on 7/25/2016.
+ */
+public class GlobalVariable extends Application {
+
+    boolean checkLogin ;
+
+    public boolean getCheckLogin (){
+
+        return checkLogin;
+    }
+
+    public void setCheckLogin (boolean checkLogin){
+
+        this.checkLogin = checkLogin;
+    }
+}
