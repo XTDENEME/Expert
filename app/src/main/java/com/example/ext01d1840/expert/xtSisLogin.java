92978bafb267467f020b68cc60c2a88b80add617
aytulyesil <aytulyesil@gmail.com>
2016-07-25 09:32:24 +0300

    login sayfası çalışması

--- app/src/main/java/com/example/ext01d1840/expert/xtSisLogin.java
+++ app/src/main/java/com/example/ext01d1840/expert/xtSisLogin.java
@@ -2,20 +2,25 @@ package com.example.ext01d1840.expert;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.FileNameMap;
 
 public class xtSisLogin extends Fragment
  {
-
-
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -57,7 +62,75 @@ public class xtSisLogin extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_xt_sis_login, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_xt_sis_login, container, false);
+
+        final EditText user = (EditText) view.findViewById(R.id.etLoginUser);
+        final EditText password =(EditText) view.findViewById(R.id.etSifre);
+        final Button login =(Button) view.findViewById(R.id.btnGiris);
+
+
+        user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                user.setText("");
+                user.setTextColor(Color.parseColor("#556f7b"));
+
+            }
+    });
+
+
+        password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                password.setText("");
+                password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                password.setEms(10);
+                password.setTextColor(Color.parseColor("#556f7b"));
+            }
+        });
+
+        /*
+        user.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+                user.getText().clear();
+                user.setTextColor(Color.parseColor("#556f7b"));
+            }
+        });
+        */
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userText =user.getText().toString();
+                String passText =password.getText().toString();
+
+                if (user.getText().toString().equals("experteam")   ){
+
+                    if (password.getText().toString().equals("welcome")){
+
+                        Toast.makeText(getContext(),"Hoşgeldin",Toast.LENGTH_SHORT).show();
+                        Intent main = new Intent(getContext(),MainActivity.class);
+                        startActivity(main);
+
+                    }
+                    else {
+                        Toast.makeText(getContext(),"Hatalı kullanıcı/şifre",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(getContext(),"Hatalı kullanıcı/şifre",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        return view;
+
+        //return inflater.inflate(R.layout.fragment_xt_sis_login, container, false);
 
 
     }
@@ -90,10 +163,6 @@ public class xtSisLogin extends Fragment
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
-     //public void userEtOnClick(View v) {
-       //  userEt.setText("");
-     //}
 
 
 }
