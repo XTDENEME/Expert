0c5956ce00678fed1f8ff57ba89b9edf6b382b43
aytulyesil <aytulyesil@gmail.com>
2016-07-31 22:16:20 +0300

    haberler sayfası değişti

--- app/src/main/java/com/example/ext01d1840/expert/xtHaberDetay.java
+++ app/src/main/java/com/example/ext01d1840/expert/xtHaberDetay.java
@@ -1,7 +1,9 @@
 package com.example.ext01d1840.expert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import android.widget.TextView;
@@ -24,14 +26,29 @@ public class xtHaberDetay extends AppCompatActivity {
         p_detay = extras.getString("p_detay");
         p_image = extras.getInt("p_image");
 
-        ImageView detayResim =  (ImageView) findViewById(R.id.hdImage);
+        final ImageView detayResim =  (ImageView) findViewById(R.id.hdImage);
 
-        TextView baslik = (TextView) findViewById(R.id.tvBaslik);
-        TextView detay = (TextView) findViewById(R.id.tvDetay);
+        final TextView baslik = (TextView) findViewById(R.id.tvBaslik);
+        final TextView detay = (TextView) findViewById(R.id.tvDetay);
 
         detayResim.setImageResource(p_image);
         baslik.setText(p_haber);
         detay.setText(p_detay);
+
+        final ImageView sharedImage  = (ImageView) findViewById(R.id.imageShared);
+
+        sharedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND); //Share intentini oluşturuyoruz
+                sharingIntent.setType("text/plain");
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "EXPERTEAM --"+baslik.getText().toString());//share mesaj konusu
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, detayResim.getImageAlpha() +"\n"+detay.getText().toString() );//share mesaj içeriği
+                startActivity(Intent.createChooser(sharingIntent, "Paylaşmak İçin Seçiniz"));//Share intentini başlığı ile birlikte başlatıyoruz
+
+            }
+        });
 
     }
 }
